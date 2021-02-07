a15b8626df3792d6a6c9ba7fa780c300c0a8ba8a
Bryan Alexander Pazmiño Gordon <75285255+bryalex98@users.noreply.github.com>
2021-02-07 18:59:04 -0500

    Polymorphism Director with Administrator

--- 06-Code/Prototype_virtual_ID/src/ec/edu/espe/simulador/model/Director.java
+++ 06-Code/Prototype_virtual_ID/src/ec/edu/espe/simulador/model/Director.java
@@ -1,113 +1,21 @@
 package ec.edu.espe.simulador.model;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+
 import java.util.Scanner;
 
 /**
  *
  * @author Developers
  */
-public class Director {
+public class Director extends Administrator{
 
-    String career;
-    String name;
-    String faculty;
-    int password = 1234;
-    String id;
+    private String career;
+    private String faculty;
+    private int password = 1234;
+    private String id;
+    private Student student;
 
-    public void addStudent() {
-
-        Scanner ans1 = new Scanner(System.in);
-        System.out.println("\n ");
-        System.out.println("Are you a career Director?");
-        System.out.print("Yes(Y) or No(N): ");
-        char answer1 = ans1.next().charAt(0);
-        ans1.nextLine();
-
-        if (answer1 == 'Y' || answer1 == 'y') {
-
-            System.out.println("Give me your name: ");
-            name = ans1.nextLine();
-            System.out.print("What career are you director of?: ");
-            faculty = ans1.nextLine();
-            System.out.print("Enter your password: ");
-            password = ans1.nextInt();
-            ans1.nextLine();
-
-            if (password == 1234) {
-
-                try {
-                    System.out.println(" ");
-                    System.out.println("\t1) Add Student");
-                    System.out.println("\t2) Exit");
-                    int option = ans1.nextInt();
-
-                    switch (option) {
-
-                        case 1:
-                            File file = new File("Requests.csv");
-                            Scanner input = new Scanner(System.in);
-                            file.createNewFile();
-                            FileWriter write = new FileWriter(file, true);
-                            PrintWriter line = new PrintWriter(write);
-
-                            System.out.print("Enter the student's id: ");
-                            String id = input.nextLine();
-                            System.out.print("Enter the student's name: ");
-                            String name = input.nextLine();
-                            System.out.print("Enter the student's age: ");
-                            int age = input.nextInt();
-                            input.nextLine();
-                            System.out.print("Enter the student's email: ");
-                            String email = input.nextLine();
-                            System.out.print("Enter the student's address: ");
-                            String address = input.nextLine();
-                            System.out.print("Enter the student's phone: ");
-                            int phone = input.nextInt();
-                            input.nextLine();
-                            System.out.print("Enter the student's career: ");
-                            career = input.nextLine();
-                            System.out.println("===========================================================");
-                            System.out.println("Your request has been successfully saved!!");
-                            System.out.println("This request has to be read and added to the database by the "
-                                    + "administrator");
-                            System.out.println("  BE PATTIENT ;) ");
-                            System.out.println("===========================================================");
-
-                            System.out.println(" ");
-                            String saveData = (id + "," + name + "," + age + "," + email + "," + address + ","
-                                    + phone + "," + career);
-                            line.print(saveData);
-                            line.close();
-                            write.close();
-
-                            break;
-
-                        case 2:
-                            System.out.println("Thanks");
-
-                            break;
-                    }
-
-                } catch (IOException ex) {
-
-                }
-
-            } else {
-                System.out.println("Password Incorrect!!!");
-            }
-        }
-            if (answer1 == 'N' || answer1 == 'n') {
-                System.out.println("Only career directors can use this field!!");
-                System.out.println(" ");
-
-            }
-        }
-
-
+    
 
     public void approveMotion() {
 
@@ -129,5 +37,93 @@ public class Director {
 
     }
 
-}
+    public Director(String career, String faculty, String id, Student student, String name) {
+        super(name);
+        this.career = career;
+        this.faculty = faculty;
+        this.id = id;
+        this.student = student;
+    }
+    
+    
+    public Director(){
+        
+    }
 
+    /**
+     * @return the career
+     */
+    public String getCareer() {
+        return career;
+    }
+
+    /**
+     * @param career the career to set
+     */
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    /**
+     * @return the faculty
+     */
+    public String getFaculty() {
+        return faculty;
+    }
+
+    /**
+     * @param faculty the faculty to set
+     */
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    /**
+     * @return the password
+     */
+    public int getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    /**
+     * @return the id
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the student
+     */
+    public Student getStudent() {
+        return student;
+    }
+
+    /**
+     * @param student the student to set
+     */
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    
+    
+    
+    
+    
+
+}
